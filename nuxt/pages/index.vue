3777dd0e4da5abbc7a9f798163ff3520161f1961
steinjun0 <1142308@naver.com>
2021-10-24 18:32:38 +0900

    add transition code

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,19 +1,52 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <v-row justify="center" align="center">
+    <v-col>
+      <v-btn @click="switchShow">{{ show }}</v-btn>
+      <transition name="fade">
+        <v-card
+          v-if="show"
+          width="160px"
+          height="90px"
+          style="padding: 16px"
+          class="d-flex justify-center align-center"
+        >
+          <span>안녕하세요</span>
+        </v-card>
+      </transition>
     </v-col>
   </v-row>
 </template>
+<script>
+import { defineComponent, ref, onMounted } from "@vue/composition-api";
+export default defineComponent({
+  setup() {
+    let show = ref(false);
+    const delayFunction = (func, input = 100) => {
+      return () => {
+        setTimeout(func, input);
+      };
+    };
+    const switchShow = (input = undefined) => {
+      if (typeof input !== "boolean") {
+        show.value = !show.value;
+      } else {
+        show.value = input;
+      }
+    };
+    onMounted(delayFunction(switchShow, 1000));
+    return { show, switchShow };
+  },
+});
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
