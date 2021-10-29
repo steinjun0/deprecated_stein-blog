607bbc11357ddc1c55f9d4994be5e9e64424c9ac
steinjun0 <1142308@naver.com>
2021-10-29 18:00:57 +0900

    좌측 배치들 컴포넌트화

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -6,26 +6,25 @@
         <span>안녕하세요</span>
       </TransientCard> -->
     </v-col>
-    <v-col cols="12">
+    <v-col cols="12" style="margin-bottom: 50px">
       <span style="font-size: 144px">stein.log</span>
     </v-col>
     <v-col md="4" style="border-right: solid #c4c4c4 1px">
-      <CardPostCard
-        v-for="(data, index) in postCardsData"
-        :key="index"
-        :category="data.category"
-        :title="data.title"
-        :subtitle="data.subtitle"
-        :style="index === postCardsData.length - 1 ? '' : 'margin-bottom: 56px'"
-      />
+      <LeftPostCards />
     </v-col>
-    <v-col md="4"> menu </v-col>
+    <v-col md="4" style="height: 600px">
+      <CanvasCircleMenu />
+    </v-col>
     <v-col md="4"> visual content </v-col>
   </v-row>
 </template>
 <script>
 import { defineComponent, ref, onMounted } from '@vue/composition-api'
+import LeftPostCards from './index/LeftPostCards.vue'
 export default defineComponent({
+  components: {
+    LeftPostCards,
+  },
   setup() {
     const show = ref(false)
     const delayFunction = (func, input = 100) => {
@@ -44,25 +43,7 @@ export default defineComponent({
       delayFunction(switchShow, 1000)
     })
 
-    let postCardsData = ref([])
-    postCardsData = [
-      {
-        category: '[Django,Docker]',
-        title: ['gunicorn에', 'vscode debugger 붙이기'],
-        subtitle: '개발자여, 조금 더 편하게 살아보자',
-      },
-      {
-        category: '[Vue 2]',
-        title: ['v-model과 v-bind.sync', '그리고 Vue3'],
-        subtitle: '다가오는 Vue3와 변화되는 문법',
-      },
-      {
-        category: '[Docker]',
-        title: ['docker compose와', 'Monolothic Server 구축'],
-        subtitle: '환경 구축으로부터의 탈출',
-      },
-    ]
-    return { show, switchShow, postCardsData }
+    return { show, switchShow }
   },
 })
 </script>
---
+++ nuxt/pages/index/LeftPostCards.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <CardPostCard
+      v-for="(data, index) in postCardsData"
+      :key="index"
+      :category="data.category"
+      :title="data.title"
+      :subtitle="data.subtitle"
+      :style="index === postCardsData.length - 1 ? '' : 'margin-bottom: 56px'"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    let postCardsData = ref([])
+    postCardsData = [
+      {
+        category: '[Django,Docker]',
+        title: ['gunicorn에', 'vscode debugger 붙이기'],
+        subtitle: '개발자여, 조금 더 편하게 살아보자',
+      },
+      {
+        category: '[Vue 2]',
+        title: ['v-model과 v-bind.sync', '그리고 Vue3'],
+        subtitle: '다가오는 Vue3와 변화되는 문법',
+      },
+      {
+        category: '[Docker]',
+        title: ['docker compose와', 'Monolothic Server 구축'],
+        subtitle: '환경 구축으로부터의 탈출',
+      },
+    ]
+    return { postCardsData }
+  },
+})
+</script>
+
+
+<style>
+</style>
