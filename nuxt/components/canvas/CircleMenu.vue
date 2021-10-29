272c025abfcd5677ae559c28e67d5cbbd7d38a40
steinjun0 <1142308@naver.com>
2021-10-29 18:35:44 +0900

    canvas 반응형 추가

--- nuxt/components/canvas/CircleMenu.vue
+++ nuxt/components/canvas/CircleMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 100%; height: 100%">
     <canvas id="circle-menu" style="width: 100%; height: 100%"></canvas>
-    {{ windowWidth }}
+    {{  }}
   </div>
 </template>
 
@@ -10,34 +10,46 @@ import { defineComponent, onMounted, ref } from '@vue/composition-api'
 export default defineComponent({
   setup() {
     let ctx
+    const canvasWidth = ref(0)
+    const canvasHeight = ref(0)
 
-    const getCanvasContext = () => {
+    const getCanvas = () => {
       const canvas = document.getElementById('circle-menu')
       const ctx = canvas.getContext('2d')
 
-      const { width, height } = canvas.getBoundingClientRect()
-      canvas.width = width
-      canvas.height = height
-      return ctx
+      const rect = canvas.getBoundingClientRect()
+      canvasWidth.value = rect.width
+      canvasHeight.value = rect.height
+      canvas.width = canvasWidth.value
+      canvas.height = canvasHeight.value 
+
+      window.addEventListener('resize', () => {
+        const rect = canvas.getBoundingClientRect()
+        canvasWidth.value = rect.width
+        canvasHeight.value = rect.height
+        canvas.width = canvasWidth.value
+        canvas.height = canvasHeight.value 
+        drawCanvas(canvas.getContext('2d'))
+      })
+      return {canvas, ctx}
     }
     const drawCanvas = (ctx) => {
       ctx.beginPath()
-      ctx.arc(75, 75, 200, 0, Math.PI * 2, true) // Outer circle
+      ctx.arc(canvasWidth.value/2, canvasHeight.value/2, canvasWidth.value/2 - 2, 0, Math.PI * 2, true) // Outer circle
       ctx.fillStyle = '#c4c4c4'
       ctx.fill()
       ctx.stroke()
     }
-    const windowWidth = ref(0)
     const mounted = () => {
-      ctx = getCanvasContext()
+      const tempCanvas = getCanvas()
+      ctx = tempCanvas.ctx
+
       drawCanvas(ctx)
-      window.addEventListener('resize', () => {
-        windowWidth.value = window.innerWidth
-      })
+      
     }
     onMounted(mounted)
 
-    return { windowWidth }
+    return {  }
   },
 })
 </script>
