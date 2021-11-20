f60990d15a10f30c59c4aeb1ae0bb56f78021054
steinjun0 <1142308@naver.com>
2021-11-20 16:33:45 +0900

    blog model 추가

--- django/webcompose/blog/models.py
+++ django/webcompose/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    sub_title = models.CharField(max_length=200)
+    html = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
+
