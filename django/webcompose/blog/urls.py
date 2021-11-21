b23cf3d0fbf9f8e54d67dc2d1509e7cc5dafb55c
steinjun0 <1142308@naver.com>
2021-11-21 13:19:17 +0900

    viewset router 복수형 ->단수형으로 수정

--- django/webcompose/blog/urls.py
+++ django/webcompose/blog/urls.py
@@ -84,9 +84,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'posts', PostViewSet)
-router.register(r'categories', CategoryViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'post', PostViewSet)
+router.register(r'category', CategoryViewSet)
 
 # router.register(r'companys', parsed_data_view.CompanyViewSet)
 
