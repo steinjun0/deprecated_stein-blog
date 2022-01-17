9f73fdeff2395ebd5fbc13efa168ddd54085fcb7
steinjun0 <1142308@naver.com>
2022-01-17 11:08:31 +0900

    django gunicorn debugger setting - poetry local install option

--- django/webcompose/webcompose/urls.py
+++ django/webcompose/webcompose/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+print('hello')
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/blog/', include('blog.urls')),
