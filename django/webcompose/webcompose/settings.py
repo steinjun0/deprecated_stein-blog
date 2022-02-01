cd7054e644446410cc1ef1a762fdd7f3c64185f9
steinjun0 <1142308@naver.com>
2022-02-01 00:14:59 +0000

    update for server

--- django/webcompose/webcompose/settings.py
+++ django/webcompose/webcompose/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "HOST": os.environ.get("SQL_HOST", "postgres"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
